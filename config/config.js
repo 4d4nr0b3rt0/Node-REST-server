cfe33ffc887ccdbddfd272b37e779e8a5653cf53
Adan Rodriguez <adanroberto_rm@yahoo.com.mx>
2020-09-03 22:12:27 -0500

    Rest Server cafe

--- config/config.js
+++ config/config.js
@@ -14,7 +14,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost/cafe';
 } else {
-    urlDB = 'mongodb+srv://fibcer_adan:ynvZom3NRWecZR2Q@cluster0.d3mdo.mongodb.net/test';
+    urlDB = 'mongodb+srv://fibcer_adan:ynvZom3NRWecZR2Q@cluster0.d3mdo.mongodb.net/cafe';
 }
 
 process.env.localURL = urlDB;
